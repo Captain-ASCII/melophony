a675ba63bb282ce0003a4257a7fd5f16ce87741f
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2019-06-13 00:04:45 +0200

    basic API

---
+++ index.js
@@ -0,0 +1,28 @@
+import Express from "express";
+import { exec } from "child_process";
+
+const App = Express();
+
+App.get('/:videoId', function (request, response) {
+    console.log(request.params.videoId);
+    exec(`ytdl -i https://www.youtube.com/watch?v=${request.params.videoId} | grep m4a`, (error, stdout, stderr) => {
+        if (error) {
+            response.send(`Error: ${error}`);
+            return;
+        }
+        exec(`ytdl -q 140 --print-url https://www.youtube.com/watch?v=${request.params.videoId}`, (error, stdout, stderr) => {
+            if (error) {
+                response.send(`Error: ${error}`);
+            }
+            response.send({ url: stdout });
+        });
+    });
+});
+
+App.get("/.*", (request, response) => {
+    response.send("Fallback");
+});
+
+App.listen(8081, function () {
+    console.log('Example app listening on port 3000!')
+});
