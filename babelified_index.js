0dd006761b89d6c1ef9115b5dd63bf0f5e07ca54
Daniel Ben Lulu <daniel.ben-lulu@PC13884-BES.besancon.parkeon.com>
2019-06-13 14:46:55 +0200

    Add the different commands to manage several files

--- babelified_index.js
+++ babelified_index.js
@@ -4,40 +4,119 @@ var _express = _interopRequireDefault(require("express"));
 
 var _fs = _interopRequireDefault(require("fs"));
 
+var _path = _interopRequireDefault(require("path"));
+
+var _ytdlCore = _interopRequireDefault(require("ytdl-core"));
+
 var _child_process = require("child_process");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var App = (0, _express["default"])();
 var PORT = 1789;
-var FILE = "file.m4a";
-App.use("/get", _express["default"]["static"](FILE));
-App.get("/clear", function (request, response) {
-  _fs["default"].unlinkSync(FILE);
+var DATA_FILE = "data.json";
+var State = {
+  UNAVAILABLE: "unavailable",
+  DOWNLOADING: "downloading",
+  AVAILABLE: "available",
+  DELETED: "deleted"
+};
+var files = {};
 
+try {
+  files = JSON.parse(_fs["default"].readFileSync(DATA_FILE, "utf8"));
+} catch (exception) {
+  console.warn("Unable to get previous data, restarting from zero");
+}
+
+function deleteFile(id) {
+  if (_fs["default"].existsSync("".concat(id, ".m4a"))) {
+    _fs["default"].unlinkSync("".concat(id, ".m4a"));
+  }
+}
+
+function save() {
+  _fs["default"].writeFile(DATA_FILE, JSON.stringify(files), "utf8");
+}
+
+App.put('/:videoId', function (request, response) {
+  if (!files[request.params.videoId]) {
+    files[request.params.videoId] = {
+      id: request.params.videoId,
+      size: 1,
+      state: State.UNAVAILABLE
+    };
+  }
+
+  save();
+  console.log("Add ".concat(request.params.videoId, " to files"));
   response.send({
-    state: "deleted"
+    added: request.params.videoId
   });
-});
-App.get('/:videoId', function (request, response) {
-  console.log(request.params.videoId);
-  (0, _child_process.exec)("ytdl -i https://www.youtube.com/watch?v=".concat(request.params.videoId, " | grep m4a"), function (error, stdout, stderr) {
-    if (error || typeof stdout != "string") {
+
+  _ytdlCore["default"].getInfo("https://www.youtube.com/watch?v=".concat(request.params.videoId), {}, function (error, info) {
+    if (error) {
       response.send("Error: ".concat(error));
-      return;
     }
 
-    var quality = stdout.match(/[0-9]+/);
-    console.warn(quality);
-    (0, _child_process.exec)("ytdl -q ".concat(quality[0], " https://www.youtube.com/watch?v=").concat(request.params.videoId, " > ").concat(FILE), function (error, stdout, stderr) {
-      if (error) {
-        response.send("Error: ".concat(error));
-      }
+    for (var i in info.formats) {
+      if (info.formats[i].container == "m4a") {
+        var format = info.formats[i];
+        console.log("Found information for ".concat(request.params.videoId, ": [ itag: ").concat(format.itag, ", length: ").concat(format.clen, " ]"));
+        files[request.params.videoId].size = format.clen;
+        files[request.params.videoId].state = State.DOWNLOADING;
+        save();
+        (0, _child_process.exec)("ytdl -q ".concat(format.itag, " https://www.youtube.com/watch?v=").concat(request.params.videoId, " > ").concat(request.params.videoId, ".m4a"), function (error, stdout, stderr) {
+          if (error) {
+            response.send("Error: ".concat(error));
+          }
 
-      response.send({
-        done: true
-      });
+          files[request.params.videoId].state = State.AVAILABLE;
+          save();
+          console.log("Download done for ".concat(request.params.videoId));
+          setTimeout(function (_) {
+            return deleteFile(request.params.videoId);
+          }, 300000);
+        });
+      }
+    }
+  });
+});
+App.get("/state/:videoId", function (request, response) {
+  var progress = -1;
+
+  if (files[request.params.videoId]) {
+    if (_fs["default"].existsSync("".concat(request.params.videoId, ".m4a"))) {
+      var fileSize = _fs["default"].statSync("".concat(request.params.videoId, ".m4a")).size;
+
+      progress = fileSize / files[request.params.videoId].size;
+    }
+
+    response.send({
+      id: files[request.params.videoId].id,
+      state: files[request.params.videoId].state,
+      progress: Math.round(progress * 100)
     });
+  } else {
+    response.send({
+      error: "id not found"
+    });
+  }
+});
+App.get("/status/:videoId", function (request, response) {
+  response.send({
+    state: !files[request.params.videoId] || files[request.params.videoId].state != State.AVAILABLE ? State.UNAVAILABLE : State.AVAILABLE
+  });
+});
+App.use("/get/:videoId", function (request, response) {
+  response.sendFile(_path["default"].join(__dirname, "".concat(request.params.videoId, ".m4a")));
+});
+App["delete"]("/:videoId", function (request, response) {
+  deleteFile(request.params.videoId);
+  files[request.params.videoId].state = State.DELETED;
+  save();
+  response.send({
+    state: "deleted"
   });
 });
 App.get("/.*", function (request, response) {
--- index.js
+++ index.js
@@ -1,35 +1,113 @@
 import Express from "express";
 import FileSystem from "fs";
+import Path from "path";
+import YTDL from "ytdl-core";
 import { exec } from "child_process";
 
 const App = Express();
 
 const PORT = 1789;
-const FILE = "file.m4a";
+const DATA_FILE = "data.json";
 
-App.use("/get", Express.static(FILE));
+const State = {
+    UNAVAILABLE: "unavailable",
+    DOWNLOADING: "downloading",
+    AVAILABLE  : "available",
+    DELETED    : "deleted",
+};
 
-App.get("/clear", function (request, response) {
-    FileSystem.unlinkSync(FILE);
-    response.send({ state: "deleted" });
+let files = {};
+try {
+    files = JSON.parse(FileSystem.readFileSync(DATA_FILE, "utf8"));
+} catch (exception) {
+    console.warn(`Unable to get previous data, restarting from zero`);
+}
+
+function deleteFile(id) {
+    if (FileSystem.existsSync(`${id}.m4a`)) {
+        FileSystem.unlinkSync(`${id}.m4a`);
+    }
+}
+
+function save() {
+    FileSystem.writeFile(DATA_FILE, JSON.stringify(files), "utf8");
+}
+
+App.put('/:videoId', function (request, response) {
+
+    if (!files[request.params.videoId]) {
+        files[request.params.videoId] = {
+            id: request.params.videoId,
+            size: 1,
+            state: State.UNAVAILABLE
+        };
+    }
+    save();
+    console.log(`Add ${request.params.videoId} to files`);
+
+    response.send({ added: request.params.videoId });
+
+    YTDL.getInfo(`https://www.youtube.com/watch?v=${request.params.videoId}`, {}, (error, info) => {
+        if (error) {
+            response.send(`Error: ${error}`);
+        }
+
+        for (let i in info.formats) {
+            if (info.formats[i].container == "m4a") {
+                let format = info.formats[i];
+                console.log(`Found information for ${request.params.videoId}: [ itag: ${format.itag}, length: ${format.clen} ]`);
+
+                files[request.params.videoId].size = format.clen;
+                files[request.params.videoId].state = State.DOWNLOADING;
+                save();
+                exec(`ytdl -q ${format.itag} https://www.youtube.com/watch?v=${request.params.videoId} > ${request.params.videoId}.m4a`, (error, stdout, stderr) => {
+                    if (error) {
+                        response.send(`Error: ${error}`);
+                    }
+                    files[request.params.videoId].state = State.AVAILABLE;
+                    save();
+                    console.log(`Download done for ${request.params.videoId}`);
+
+                    setTimeout(_ => deleteFile(request.params.videoId), 300000);
+                });
+            }
+        }
+    });
 });
 
-App.get('/:videoId', function (request, response) {
-    console.log(request.params.videoId);
-    exec(`ytdl -i https://www.youtube.com/watch?v=${request.params.videoId} | grep m4a`, (error, stdout, stderr) => {
-        if (error || typeof stdout != "string") {
-            response.send(`Error: ${error}`);
-            return;
+App.get("/state/:videoId", function (request, response) {
+    let progress = -1;
+
+    if (files[request.params.videoId]) {
+        if (FileSystem.existsSync(`${request.params.videoId}.m4a`)) {
+            const fileSize = FileSystem.statSync(`${request.params.videoId}.m4a`).size;
+            progress = fileSize / files[request.params.videoId].size;
         }
-        let quality = stdout.match(/[0-9]+/);
-        console.warn(quality);
-        exec(`ytdl -q ${quality[0]} https://www.youtube.com/watch?v=${request.params.videoId} > ${FILE}`, (error, stdout, stderr) => {
-            if (error) {
-                response.send(`Error: ${error}`);
-            }
-            response.send({ done: true });
+        response.send({
+            id: files[request.params.videoId].id,
+            state: files[request.params.videoId].state,
+            progress: Math.round(progress * 100)
         });
+    } else {
+        response.send({ error: "id not found" });
+    }
+});
+
+App.get("/status/:videoId", function (request, response) {
+    response.send({
+        state: (!files[request.params.videoId] || files[request.params.videoId].state != State.AVAILABLE) ? State.UNAVAILABLE : State.AVAILABLE
     });
+});
+
+App.use("/get/:videoId", function (request, response) {
+    response.sendFile(Path.join(__dirname, `${request.params.videoId}.m4a`));
+});
+
+App.delete("/:videoId", function (request, response) {
+    deleteFile(request.params.videoId);
+    files[request.params.videoId].state = State.DELETED;
+    save();
+    response.send({ state: "deleted" });
 });
 
 App.get("/.*", (request, response) => {
