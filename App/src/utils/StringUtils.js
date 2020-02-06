915ca9ff09fe7329d3a92c6f2a8743bfb51611c8
d <d>
2020-02-06 13:00:21 +0100

    Rename utils into StringUtils

--- App/src/utils/StringUtils.js
+++ App/src/utils/StringUtils.js
@@ -3,7 +3,7 @@ export default class Utils {
   static generateId() {
     let result = ''
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    
+
     for (let i = 0; i < 8; i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length))
     }
