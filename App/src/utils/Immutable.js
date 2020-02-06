fcdbfc32037c6dd6642e1c9a01fc1bf4804761f4
d <d>
2020-02-06 13:00:52 +0100

    Fix immutable arrays methods

--- App/src/utils/Immutable.js
+++ App/src/utils/Immutable.js
@@ -13,7 +13,7 @@ class Arrays {
   }
 
   static remove(array, condition) {
-    return array.filter(condition)
+    return array.filter(element => !condition(element))
   }
 
   static add(array, element) {
@@ -21,11 +21,11 @@ class Arrays {
   }
 
   static reverse(array) {
-    return Arrays.copy().reverse()
+    return Arrays.copy(array).reverse()
   }
 
   static sort(array, sortFct) {
-    return Arrays.copy().sort(sortFct)
+    return Arrays.copy(array).sort(sortFct)
   }
 }
 
