926c0c29fa05e24640327ffb8cbe2ba7a25179ac
d <d>
2020-02-21 17:20:07 +0100

    Add ability to shrink menu

--- App/src/App.js
+++ App/src/App.js
@@ -1,4 +1,5 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import { useDispatch } from 'react-redux'
 
 import { hot } from 'react-hot-loader'
@@ -11,7 +12,6 @@ import ModificationScreen from 'screens/ModificationScreen'
 import TracksScreen from 'screens/TracksScreen'
 
 import { setApiManager } from 'actions/Managers'
-import { setInConfiguration } from 'actions/Configuration'
 
 import ApiManager from 'utils/ApiManager'
 import MediaManager from 'utils/MediaManager'
@@ -22,10 +22,33 @@ import ConfirmOverlay from 'components/ConfirmOverlay'
 import { ConfigurationSwitch, SwitchState } from 'components/Switch'
 import NotificationToaster from 'components/NotificationToaster'
 
+
+const MenuLink = ({ title, path, icon }) => {
+  return (
+    <Link to={path} >
+      <div className="menuLink button" >
+        <i className={`fa fa-${icon}`} />
+        <p className="buttonTitle" >{ title }</p>
+      </div>
+    </Link>
+  )
+}
+
+MenuLink.propTypes = {
+  title: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+}
+
+
+
+
 const App = () => {
   const dispatch = useDispatch()
 
   const apiManager = selectApiManager()
+
+  const [ menuState, setMenu ] = useState('closed')
 
   useEffect(() => {
     dispatch(setApiManager(new ApiManager('http://localhost:1958')))
@@ -33,13 +56,15 @@ const App = () => {
 
   const synchronize = useCallback(() => apiManager.get('synchronize'))
 
+  const handleMenuSwitch = useCallback(() => setMenu(prev => prev === 'opened' ? 'closed' : 'opened'))
+
   return(
     <Router>
       <div className="App">
         <div className="main-container">
-          <div className="sidebar left" >
-            <Link to="/tracks" ><div className="button">Titles</div></Link>
-            <Link to="/artists" ><div className="button">Artists</div></Link>
+          <div className={`sidebar left ${menuState}`} >
+            <MenuLink path="/tracks" title="Tracks" icon="music" />
+            <MenuLink path="/artists" title="Artists" icon="user-friends" />
             <div id="toaster">
               <div id="toasterText">?!</div>
             </div>
@@ -60,14 +85,17 @@ const App = () => {
           </div>
         </div>
         <div id="header">
-          <Link to="/" >
-            <div id="AppHeader">
-              <div className="logo" >
-                <img src="/img/melophony.png" style={{ height: '100%' }} />
+          <div id="headerMenu" >
+            <i className="fa fa-bars fa-2x icon button" onClick={handleMenuSwitch} />
+            <Link to="/" >
+              <div id="AppHeader">
+                <div className="logo" >
+                  <img src="/img/melophony.png" style={{ height: '100%' }} />
+                </div>
+                <h1>Melophony</h1>
               </div>
-              <h1>Melophony</h1>
-            </div>
-          </Link>
+            </Link>
+          </div>
           <div id="headerActions">
             <i onClick={synchronize} className="fa fa-download icon button" />
             <ConfigurationSwitch
