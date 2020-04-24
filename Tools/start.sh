20e3b44f44df55ef5fe36265f49c5ed900ef3744
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2020-04-24 18:28:53 +0100

    Change Melophony run tools

--- Tools/start.sh
+++ Tools/start.sh
@@ -1,7 +1,9 @@
 cd ~/Documents/melophony-api/Server/
 sudo nohup npm start &
-echo $! > ~/Documents/melophony-api/Tools/current_melophony_pid.txt
+#echo $! > $HOME/Documents/melophony-api/Tools/current_melophony_pid.txt
 
 cd ~/Documents/melophony-api/App/
 sudo nohup npm run production &
-echo $! >> ~/Documents/melophony-api/Tools/current_melophony_pid.txt
+#echo $! >> $HOME/Documents/melophony-api/Tools/current_melophony_pid.txt
+
+#cat $HOME/Documents/melophony-api/Tools/current_melophony_pid.txt
