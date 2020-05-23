3cf22acc35f92cc0437afbf7681c0627c1040874
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2020-05-23 19:31:03 +0200

    Improve user drawer display

--- App/src/components/UserDrawer.tsx
+++ App/src/components/UserDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import JWT from 'jwt-client'
 
@@ -8,22 +8,49 @@ import { setUser } from '@actions/App'
 import { selectUser } from '@selectors/App'
 
 import Button from '@components/Button'
+import IconButton from '@components/IconButton'
+
+class DrawerState {
+  public opened: boolean
+  public maxHeight: number
+  public icon: string
+
+  constructor(opened: boolean, maxHeight: number, icon: string) {
+    this.opened = opened
+    this.maxHeight = maxHeight
+    this.icon = icon
+  }
+
+  public static OPENED = new DrawerState(true, 500, 'chevron-down')
+  public static CLOSED = new DrawerState(false, 0, 'chevron-right')
+}
 
 const UserDrawer = (): JSX.Element => {
   const dispatch = useDispatch()
 
   const user = selectUser()
 
+  const [ drawerState, setDrawerState ] = useState(DrawerState.CLOSED)
+
   const disconnect = useCallback(() => {
     dispatch(setUser(null))
     JWT.forget()
     init()
-  }, [])
+  }, [ dispatch ])
+
+  const openClose = useCallback(() => {
+    setDrawerState(drawerState.opened ? DrawerState.CLOSED : DrawerState.OPENED)
+  }, [ drawerState ])
 
   return (
     <div id="userDrawer" >
-      <p id="userMessage" >Hello { user.getFirstName() } !</p>
-      <Button className="raised alert" icon="times-circle" onClick={disconnect} title="Disconnect" />
+      <div id="userName" >
+        <p id="userMessage" ><span className="hideWhenClosed">Hello </span>{ user.getFirstName() }</p>
+        <IconButton className="hideWhenClosed" icon={drawerState.icon} onClick={openClose} />
+      </div>
+      <div id="userSpace" style={{ maxHeight: drawerState.maxHeight }} >
+        <Button className="raised alert hideWhenClosed" icon="times-circle" onClick={disconnect} title="Disconnect" />
+      </div>
     </div>
   )
 }
