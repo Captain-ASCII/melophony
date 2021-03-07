15e9863984cfd5651c86cb397a605d8db5391b00
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2021-03-07 17:22:16 +0100

    Fix slow reaction when scrolling the track list

---
+++ App/src/hooks/LongPressHook.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export default function useLongPress(callback = () => {}, ms = 300) {
+  const [startLongPress, setStartLongPress] = useState(false)
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout
+
+    if (startLongPress) {
+      timerId = setTimeout(callback, ms);
+    } else {
+      clearTimeout(timerId);
+    }
+
+    return () => {
+      clearTimeout(timerId)
+    };
+  }, [callback, ms, startLongPress]);
+
+  return {
+    onTouchStart: () => setStartLongPress(true),
+    onTouchEnd: () => setStartLongPress(false),
+    onTouchCancel: () => setStartLongPress(false),
+    onTouchMove: () => setStartLongPress(false),
+  };
+}
