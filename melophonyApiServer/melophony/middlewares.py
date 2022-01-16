c2d66777a82730c22789b90cdcbfdcb7eaf13073
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2022-01-16 11:31:10 +0100

    Remove unused token check

--- melophonyApiServer/melophony/middlewares.py
+++ melophonyApiServer/melophony/middlewares.py
@@ -22,10 +22,6 @@ class JWTAuthenticationMiddleware:
         if request.path == '/login' or request.path == '/register' or request.path == '/favicon.ico' or request.path.startswith('/admin'):
             return self.get_response(request)
         else:
-            user_jwt = get_user(request)
-            if user_jwt.is_authenticated:
-                return self.get_response(request)
-
             token = request.META.get('HTTP_AUTHORIZATION', None)
             if token is None or token == 'null':
                 token = request.GET.get('jwt', None)
@@ -33,9 +29,9 @@ class JWTAuthenticationMiddleware:
             if token is not None and token != 'null':
                 try:
                     user_jwt = jwt.decode(token, MelophonyConfig.jwt_secret, algorithms=["HS256"])
-                    user_jwt = User.objects.get(id=user_jwt['user']['id'])
+                    user = User.objects.get(id=user_jwt['user']['id'])
                     if user_jwt is not None:
-                        request.user_id = user_jwt.id
+                        request.user = user
                         return self.get_response(request)
                 except Exception as e:
                     traceback.print_exc()
