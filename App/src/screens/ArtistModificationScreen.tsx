ac14c778cb02e85ee5d3e9051c8313331ff738ae
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2022-01-23 14:55:31 +0100

    Fix tracks modification for artist with same name

--- App/src/screens/ArtistModificationScreen.tsx
+++ App/src/screens/ArtistModificationScreen.tsx
@@ -45,7 +45,7 @@ const ArtistModificationScreen = (): JSX.Element => {
                 `Cela va effacer l'artiste "${initialName}" et affecter toutes ses musiques à l'artiste "${artist.getName()}", êtes vous sûr ?`,
                 () => {
                   tracks.forEach(track => {
-                    apiManager.put(`/track/${track.getId()}`, {artists: [sameNameArtist]})
+                    apiManager.put(`/track/${track.getId()}`, {artists: [sameNameArtist.getId()]})
                   })
                   apiManager.delete(`/artist/${artist.getId()}`)
                   history.goBack()
