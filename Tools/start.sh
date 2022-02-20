94c97062cb23e2f10103876cca8e6b54d93138e7
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2022-02-20 14:38:56 +0100

    Update start script to be usable in any install directory

--- Tools/start.sh
+++ Tools/start.sh
@@ -1,6 +1,13 @@
-cd $HOME/melophony-api/melophonyApiServer/
+#!/usr/bin/env bash
+
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+melophony_dir=${script_dir%"/Tools"}
+
+cd $melophony_dir/Server/
 source venv/bin/activate
+rm nohup.out
 nohup gunicorn -c gunicorn_conf.py melophonyApiServer.wsgi &
 
-cd $HOME/melophony-api/App/
+cd $melophony_dir/App/
+rm nohup.out
 nohup npm run production &
