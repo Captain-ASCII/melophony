01441e9c2e59b6562e873ec0362ade42c2465316
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2023-04-02 16:08:05 +0200

    Improve track bar UI

--- App/src/components/InputRange.tsx
+++ App/src/components/InputRange.tsx
@@ -1,25 +1,36 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react'
 
+import StringUtils from '@utils/StringUtils'
+
 import Track from '@models/Track'
 import MediaManager from '@utils/MediaManager'
 import { selectMediaManager } from '@selectors/App'
 
-const getPercentage = (value: number, track: Track): number => {
-  return (value / track.getDuration()) * 100
+const getPercentage = (range: HTMLInputElement, value: number, track: Track, inverted = false): string => {
+  let base = 100, unit = '%', diff = 0
+  if (range != null)  {
+    base = range.offsetWidth, unit = 'px', diff = 9
+  }
+  let percentage = (value / track.getDuration()) * (base - diff)
+  if (inverted) {
+    percentage = base - percentage
+  }
+  return `${percentage}${unit}`
 }
 
 let intervalHandle: any
 
 const InputRange = ({ track, multiRange, disabled = false, asReader, onStartSet, onEndSet }:
-  { track: Track | null; multiRange?: boolean; asReader?: boolean; disabled?: boolean;
+  { track: Track | null; multiRange?: boolean; asReader?: boolean; disabled?: boolean; id?: string;
     onStartSet?: (v: number) => void; onEndSet?: (v: number) => void }): JSX.Element | null => {
 
   if (track) {
-    const [ leftValue, setLeftValue ] = useState(getPercentage(track.getStartTime(), track))
-    const [ rightValue, setRightValue ] = useState(asReader ? 100 : 100 - getPercentage(track.getEndTime(), track))
-
     const mediaManager = selectMediaManager()
     const trackerRef = useRef<HTMLInputElement>(null)
+
+    const [ id, _ ] = useState(StringUtils.generateId())
+    const [ leftValue, setLeftValue ] = useState(getPercentage(trackerRef.current, track.getStartTime(), track))
+    const [ rightValue, setRightValue ] = useState(asReader ? 100 : getPercentage(trackerRef.current, track.getEndTime(), track, true))
 
     let handler = null
     const max = asReader ? track.getEndTime() : track.getDuration()
@@ -31,27 +42,30 @@ const InputRange = ({ track, multiRange, disabled = false, asReader, onStartSet,
       handler = useCallback(event => {
         if (element) {
           player.currentTime = event.target.value
-          setRightValue(100 - getPercentage(player.currentTime, track))
+          setRightValue(getPercentage(trackerRef.current, player.currentTime, track, true))
         }
       }, [ element, player.currentTime, track ])
+
+      const setTrackBar = function(value: number) {
+        if (trackerRef && trackerRef.current) {
+          trackerRef.current.value = value.toString()
+          setRightValue(getPercentage(trackerRef.current, value, track, true))
+        }
+      }
 
       useEffect(() => {
         if (intervalHandle) {
           clearInterval(intervalHandle)
         }
-        intervalHandle = setInterval(() => {
-          if (trackerRef && trackerRef.current) {
-            trackerRef.current.value = player.currentTime.toString()
-            setRightValue(100 - getPercentage(player.currentTime, track))
-          }
-        }, 200)
-      }, [ track, player.currentTime ])
+        intervalHandle = setInterval(() => setTrackBar(player.currentTime), 500)
+        setTrackBar(track.getStartTime())
+      }, [ track ])
 
     } else {
       handler = useCallback(event => {
         const value = event.target.value
         mediaManager.playExtract(value)
-        setRightValue(100 - getPercentage(value, track))
+        setRightValue(getPercentage(trackerRef.current, value, track, true))
         onEndSet(Math.max(0, parseInt(value) + (MediaManager.EXTRACT_DURATION / 1000)))
       }, [ onEndSet, mediaManager, track ])
     }
@@ -61,15 +75,17 @@ const InputRange = ({ track, multiRange, disabled = false, asReader, onStartSet,
     const handleSecondaryInput = useCallback(event => {
       const value = event.target.value
       mediaManager.playExtract(value)
-      setLeftValue(getPercentage(value, track))
+      setLeftValue(getPercentage(trackerRef.current, value, track))
       onStartSet(parseInt(value))
     }, [ onStartSet, mediaManager, track ])
 
     const disabledClass = disabled ? 'disabled' : ''
 
     return (
-      <div id="tracker" className="multi-range" >
-        <div className="trackSlider" style={{ left: '0%', right: '0%' }}  />
+      <div id={id} className="tracker multi-range" >
+        <div className="trackSlider" style={{ left: '0%', right: '0%' }}>
+          <div className={`trackBar ${disabledClass}`} style={{left: leftValue, right: rightValue}}  />
+        </div>
         {
           multiRange &&
           <input
@@ -81,9 +97,8 @@ const InputRange = ({ track, multiRange, disabled = false, asReader, onStartSet,
         <input
           ref={trackerRef} className={`mainRange ${multiRange ? 'rangeDeactivate' : ''} ${disabledClass}`} type='range'
           min={0} max={max} defaultValue={defaultValue} disabled={disabled}
-          step="0.5" onInput={handleMainInput}
+          step={asReader ? "1" : "0.1"} onInput={handleMainInput}
         />
-        <div className={`trackBar ${disabledClass}`} style={{left: `${leftValue}%`, right: `${rightValue}%`}}  />
       </div>
       )
   }
