da9027a0362624420dbe70eeb6f5af60d49062b0
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2023-07-07 11:31:27 +0200

    Do not associate related objects in results
    Only ids are kept, this is then easier to change the data in the webapp.

--- Server/melophony/serializers.py
+++ Server/melophony/serializers.py
@@ -7,38 +7,37 @@ from melophony.models import Artist, File, Playlist, Track, User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'url', 'username', 'first_name', 'last_name', 'email']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
+        extra_kwargs = {'id': {'read_only': True}}
 
 
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = ['id', 'name', 'imageUrl', 'imageName', 'user']
-        extra_kwargs = {'user': {'read_only': True}}
+        extra_kwargs = {'id': {'read_only': True}, 'user': {'read_only': True}}
 
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = ['id', 'fileId', 'state']
+        extra_kwargs = {'id': {'read_only': True}, 'fileId': {'read_only': True}}
 
 
 class TrackSerializer(serializers.ModelSerializer):
-    artists = ArtistSerializer(many=True, read_only=True)
-    file = FileSerializer(read_only=True)
 
     class Meta:
         model = Track
         fields = ['id', 'title', 'file', 'artists', 'creationDate', 'duration', 'startTime', 'endTime', 'lastPlay', 'playCount', 'rating', 'progress', 'user']
-        extra_kwargs = {'user': {'read_only': True}}
+        extra_kwargs = {'id': {'read_only': True}, 'user': {'read_only': True}}
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
-    tracks = TrackSerializer(many=True, read_only=True)
 
     class Meta:
         model = Playlist
         fields = ['id', 'name', 'tracks', 'imageUrl', 'imageName', 'user']
-        extra_kwargs = {'user': {'read_only': True}}
+        extra_kwargs = {'id': {'read_only': True}, 'user': {'read_only': True}}
 
 
